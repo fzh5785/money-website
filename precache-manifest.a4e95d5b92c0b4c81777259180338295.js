b91f30c8dffa790681fe7149952780e219824f50
ISol <599304776@qq.com>
2020-08-04 11:28:55 +0800

    deploy

---
+++ precache-manifest.a4e95d5b92c0b4c81777259180338295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8ba8d3dffac1346488",
+    "url": "/money-website/css/app.568056c8.css"
+  },
+  {
+    "revision": "a077f3714fc5d1d3891fecea10a07e3f",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "4d8ba8d3dffac1346488",
+    "url": "/money-website/js/app.db7c8613.js"
+  },
+  {
+    "revision": "412d272c2270dfcb3ff1",
+    "url": "/money-website/js/chunk-vendors.0ae02835.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
